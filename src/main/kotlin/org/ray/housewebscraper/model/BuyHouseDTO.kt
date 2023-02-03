a979ba91b090ada223c5222876fc777454bd0321
ruben <ruben.timmermans@alliander.com>
2023-02-03 20:32:00 +0100

    more testing

---
+++ src/main/kotlin/org/ray/housewebscraper/model/BuyHouseDTO.kt
@@ -0,0 +1,13 @@
+package org.ray.housewebscraper.model
+
+data class BuyHouseDTO(
+    val street: String,
+    val houseNumber: String,
+    val zipCode: String,
+    val city: String,
+    val price: String,
+    val surface: String,
+    val numberOfRooms: String,
+    val link: String,
+
+)
--- src/test/kotlin/org/ray/housewebscraper/ParserTest.kt
+++ src/test/kotlin/org/ray/housewebscraper/ParserTest.kt
@@ -1,9 +1,16 @@
 package org.ray.housewebscraper
 
-import kotlinx.coroutines.*
+import com.nimbusds.oauth2.sdk.util.StringUtils.isAlpha
+import com.nimbusds.oauth2.sdk.util.StringUtils.isNumeric
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.NodeTraversor
 import org.junit.jupiter.api.Test
+import org.ray.housewebscraper.model.BuyHouseDTO
 import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.http.codec.ClientCodecConfigurer
@@ -90,38 +97,90 @@ class ParserTest {
         val allLinks = generateSequence(1) { it + 1 }.take(
             highestPage.split("p").last().toInt()
         ).map { "https://funda.nl/koop/$CITY/0-350000/p$it" }
-//        val results = //                val whatever = webclient.get()
-////                    .uri("https://funda.nl/koop/haarlem/0-350000")
-////                    .accept(MediaType.APPLICATION_XML)
-////                    .retrieve()
-////                    .awaitBody<String>()
-//
-//            allLinks.take(1).map {
-//                async(Dispatchers.IO) {
-//                    // this is it!
-//                    println("starting $it on thread ${Thread.currentThread()}")
-//                    delay((0..10).random().toLong() * 1000)
-//                    it
-//                }
-//            }.toList()
         // search result media and search result promo add up to the total amount of ads per page, get them independently?
-        val houses = allLinks.take(1).toList().map {
+        val houses = allLinks.take(2).toList().map {
             async {
-                println("getting link $it")
+                val streetList: MutableList<String> = mutableListOf<String>()
+                val cityList: MutableList<String> = mutableListOf()
+                val houseNumberList: MutableList<String> = mutableListOf()
+                val zipCodeList: MutableList<String> = mutableListOf()
+                val squareMeterList: MutableList<String> = mutableListOf()
+                val priceList: MutableList<String> = mutableListOf()
+                val numberOfRoomsList: MutableList<String> = mutableListOf()
+                val linkList: MutableList<String> = mutableListOf()
                 val specificResult = webclient.get()
                     .uri(it)
                     .accept(MediaType.APPLICATION_XML)
                     .retrieve()
                     .awaitBody<String>()
                 val houseDocument: Document = Jsoup.parse(specificResult)
-                return@async houseDocument.select(".search-result-media a[data-object-url-tracking*=\"resultlist\"]").toList()
+                val children = houseDocument.select(".search-result-content-inner")
+                NodeTraversor.traverse({ node, _ ->
+                    val street: String
+                    val houseNumber: String
+                    val city: String
+                    val zipCode: String
+                    val squareMeters: String
+                    val price: String
+                    val numberOfRooms: String
+                    if (node is Element) {
+                        val text = node.text()
+                        when (node.className()) {
+                            "search-result__header-subtitle fd-m-none" -> {
+                                val values = text.split(" ")
+                                city = values[values.size - 1]
+                                zipCode = values[0] + values[1]
+                                zipCodeList.add(zipCode)
+                                cityList.add(city)
+                                val href = node.parent()?.attr("href")
+                                linkList.add(href!!)
+                            }
+                            "search-result__header-title fd-m-none" -> {
+                                val values = text.split(" ")
+                                val houseNumberIndex = findFirstHouseNumberIndex(values)
+                                street = values.subList(0, houseNumberIndex).joinToString(" ")
+                                houseNumber = values.subList(houseNumberIndex, values.size).joinToString("-")
+                                streetList.add(street)
+                                houseNumberList.add(houseNumber)
+//                                println("house found! $street + $houseNumber + $city + $zipCode")
+                            }
+                            "search-result-kenmerken" -> {
+                                squareMeters = node.children()[0].text().split(" ")[0]
+                                numberOfRooms = node.children()[1].text().split(" ")[0]
+                                numberOfRoomsList.add(numberOfRooms)
+                                squareMeterList.add(squareMeters)
+                            }
+                            "search-result-price" -> {
+                                price = text.split(" ")[1]
+                                priceList.add(price)
+                            }
+                            else -> {}
+                        }
+                    }
+
+                }, children)
+                val housesPerPage = zip(
+                    streetList,
+                    houseNumberList,
+                    zipCodeList,
+                    cityList,
+                    priceList,
+                    squareMeterList,
+                    numberOfRoomsList,
+                    linkList,
+                    transform = {
+                        BuyHouseDTO(it[0], it[1], it[2], it[3], it[4], it[5], it[6], it[7])
+                    })
+                housesPerPage.forEach { println("dto $it") }
+                return@async housesPerPage
+//                return@async houseDocument.select(".search-result-media a[data-object-url-tracking*=\"resultlist\"]").toList()
             }
         }.awaitAll().flatten()
-        print(houses.size)
-        houses.forEach {
-            print("hello funda $it")
-        }
-        TODO("use search-result-content-inner instead of the search result media")
+        print("hey" + houses.size)
+//        houses.forEach {
+//            print("hello funda $it")
+//        }
+//        TODO("use search-result-content-inner instead of the search result media")
     }
 
     @Test
@@ -153,4 +212,30 @@ class ParserTest {
             MimeTypeUtils.TEXT_HTML
         )
     }
+}
+
+inline fun <T, V> zip(vararg lists: List<T>, transform: (List<T>) -> V): List<V> {
+    val minSize = lists.map(List<T>::size).min()
+    val list = ArrayList<V>(minSize)
+
+    val iterators = lists.map { it.iterator() }
+    var i = 0
+    while (i < minSize) {
+        list.add(transform(iterators.map { it.next() }))
+        i++
+    }
+
+    return list
+}
+
+fun findFirstHouseNumberIndex(literal: String): Int {
+    return literal.split(" ").takeWhile { isAlpha(it) }.size - 1
+}
+
+fun findFirstHouseNumberIndex(literal: List<String>): Int {
+    return literal.takeWhile {
+        val what = isAlpha(it) or !isNumeric(it)
+        println("$it + $what")
+        what
+    }.size
 }
