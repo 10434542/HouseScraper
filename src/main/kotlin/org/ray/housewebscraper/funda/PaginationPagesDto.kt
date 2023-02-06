01ed84325a0dd86d9610ddb8072e6782102e2cdb
10434542 <ruben.timmermans@live.nl>
2023-02-06 22:51:58 +0100

    remove unnecessary dto

--- src/main/kotlin/org/ray/housewebscraper/funda/PaginationPagesDto.kt
+++
@@ -1,9 +0,0 @@
-package org.ray.housewebscraper.funda
-
-import javax.xml.bind.annotation.XmlAccessorType
-import javax.xml.bind.annotation.XmlElement
-
-@XmlAccessorType
-data class PaginationPagesDto(
-    @field:XmlElement(name = "div class=\"pagination-pages\"") val paginations : String
-)
