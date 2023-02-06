e4f934d62248937658ec1308f2166d9d5cb69aa7
10434542 <ruben.timmermans@live.nl>
2023-02-06 22:52:35 +0100

    update HouseWebClient interface

--- src/main/kotlin/org/ray/housewebscraper/model/HouseWebClient.kt
+++ src/main/kotlin/org/ray/housewebscraper/model/HouseWebClient.kt
@@ -1,8 +1,5 @@
 package org.ray.housewebscraper.model
 
 interface HouseWebClient {
-
     suspend fun getHousesByCityWithinRange(cityName: String, minimum: Long, maximum: Long) : List<BuyHouseDTO>
-
-    suspend fun getBuyHouseByCityAndPriceRange(cityName: String, range: LongRange)
 }
