fcc31f8735837c32cb834ad271424af9814ece2d
10434542 <ruben.timmermans@live.nl>
2023-02-19 21:04:41 +0100

    setup tests

---
+++ src/test/kotlin/org/ray/housewebscraper/funda/FundaClientHouseTest.kt
@@ -0,0 +1,22 @@
+package org.ray.housewebscraper.funda
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class FundaClientHouseTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun getHousesByCityWithinRange() {
+    }
+}
---
+++ src/test/kotlin/org/ray/housewebscraper/persistance/repositories/BuyHouseRepositoryImplTest.kt
@@ -0,0 +1,30 @@
+package org.ray.housewebscraper.persistance.repositories
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class BuyHouseRepositoryImplTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun insert() {
+    }
+
+    @Test
+    fun getBuyHouseById() {
+    }
+
+    @Test
+    fun getBuyHousesByCity() {
+    }
+}
