145830d17639e62b804892a3c78cd32f12d5a2e5
10434542 <ruben.timmermans@live.nl>
2023-03-07 09:03:11 +0100

    more wip, issue with webclienttest: coroutines and mocking extension functions

--- src/main/kotlin/org/ray/housewebscraper/pararius/ParariusClientHouse.kt
+++ src/main/kotlin/org/ray/housewebscraper/pararius/ParariusClientHouse.kt
@@ -5,7 +5,7 @@ import org.ray.housewebscraper.model.entities.BuyHouseDTO
 import org.ray.housewebscraper.model.interfaces.HouseWebClient
 
 class ParariusClientHouse : HouseWebClient {
-    override suspend fun getHousesByCityWithinRange(cityName: String, minimum: Long, maximum: Long):
+    override suspend fun getHousesByCityWithinRange(cityName: String, minimum: Long, maximum: Long, pages: Int):
             Either<Throwable, List<BuyHouseDTO>> {
         TODO("Not yet implemented")
     }
