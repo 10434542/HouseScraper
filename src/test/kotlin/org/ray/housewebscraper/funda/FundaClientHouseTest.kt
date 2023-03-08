5366dcd170b355f5443b16c71030e01eb2d75ba8
ruben <ruben.timmermans@alliander.com>
2023-03-08 18:31:44 +0100

    add stubresponse function

--- src/test/kotlin/org/ray/housewebscraper/funda/FundaClientHouseTest.kt
+++ src/test/kotlin/org/ray/housewebscraper/funda/FundaClientHouseTest.kt
@@ -1,12 +1,15 @@
 package org.ray.housewebscraper.funda
 
 import com.github.tomakehurst.wiremock.WireMockServer
-import io.mockk.*
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.apache.http.HttpHeaders
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.*
 import org.ray.housewebscraper.HouseWebScraperSpringApplication
 import org.ray.housewebscraper.configuration.WireMockContextInitializer
 import org.ray.housewebscraper.model.interfaces.HouseWebClient
@@ -14,6 +17,7 @@ import org.ray.housewebscraper.utility.testslices.NoMongoConfigurationPresent
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
@@ -47,12 +51,28 @@ internal class FundaClientHouseTest : NoMongoConfigurationPresent() {
         wmServer.resetAll()
     }
 
-//    @OptIn(ExperimentalCoroutinesApi::class)
-//    @Test
-//    fun getHousesByCityWithinRange(): Unit = runTest {
-//        val result = fundaClient.getHousesByCityWithinRange("haarlem", 100000L, 300000L, 1)
-//        assertThat(result.isLeft()).isEqualTo(true)
-//    }
+    private fun stubResponse(
+        url: String,
+        responseBody: String,
+        responseStatus: Int = org.springframework.http.HttpStatus.OK.value()
+    ) {
+        wmServer.stubFor(
+            get(url)
+                .willReturn(
+                    aResponse()
+                        .withStatus(responseStatus)
+                        .withBody(responseBody)
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)
+                )
+        )
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun getHousesByCityWithinRange(): Unit = runTest {
+        val result = fundaClient.getHousesByCityWithinRange("haarlem", 100000L, 300000L, 1)
+        assertThat(result.isLeft()).isEqualTo(true)
+    }
 
     @Test
     fun contextLoads() {
