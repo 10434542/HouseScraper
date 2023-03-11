1da2d161df7ac36ad4bc06eec6f30e2b329f8fa6
ruben <ruben.timmermans@alliander.com>
2023-03-11 19:06:19 +0100

    add first working example for mongodb component test

--- src/test/kotlin/org/ray/housewebscraper/persistance/repositories/MongoDatabaseComponentTest.kt
+++ src/test/kotlin/org/ray/housewebscraper/persistance/repositories/MongoDatabaseComponentTest.kt
@@ -6,35 +6,24 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.ray.housewebscraper.HouseWebScraperSpringApplication
 import org.ray.housewebscraper.model.entities.BuyHouseDocument
-import org.ray.housewebscraper.utility.testslices.MongoConfigurationPresent
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
-import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @DataMongoTest
-@ExtendWith(SpringExtension::class)
 @ComponentScan
-@ContextConfiguration(classes = [HouseWebScraperSpringApplication::class])
-@TestPropertySource(properties = ["spring.mongodb.embedded.version=4.0.12", "spring.mongodb."])
-internal class MongoDatabaseIntegrationTest {
+@ExtendWith(SpringExtension::class)
+internal class MongoDatabaseComponentTest {
 
     @Autowired
     private lateinit var template: ReactiveMongoTemplate
 
     @Autowired
     private lateinit var repository: BuyHouseRepository
-
+//
     private lateinit var document: BuyHouseDocument
 
     @BeforeEach
