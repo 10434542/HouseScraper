b7ed55845646eae11a922680baf79ee1272eb8dc
10434542 <ruben.timmermans@live.nl>
2023-05-20 15:08:13 +0200

    print something in the scheduler, lower fixedDelayString, adjust SecurityConfiguration

--- backend/src/main/kotlin/org/ray/housewebscraper/config/SecurityConfiguration.kt
+++ backend/src/main/kotlin/org/ray/housewebscraper/config/SecurityConfiguration.kt
@@ -2,6 +2,7 @@ package org.ray.housewebscraper.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
@@ -13,14 +14,14 @@ class SecurityConfiguration {
 
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        http.csrf().disable().authorizeExchange().anyExchange().permitAll()
-//        http.csrf().disable()
-//            .cors().disable()
-//            .authorizeExchange()
+        http.csrf().disable()
+            .cors().disable()
+            .authorizeExchange()
 //            .pathMatchers("/v3/api-docs/**","/swagger-resources/**",
 //                        "/webjars/**",
 //                        "/swagger-ui/**", "/swagger-ui/index.html").authenticated()
-//            .and().httpBasic(withDefaults()).formLogin(withDefaults())
+            .anyExchange().authenticated()
+            .and().httpBasic(withDefaults()).formLogin(withDefaults())
         return http.build()
     }
 }
--- backend/src/main/kotlin/org/ray/housewebscraper/service/Scheduler.kt
+++ backend/src/main/kotlin/org/ray/housewebscraper/service/Scheduler.kt
@@ -1,6 +1,7 @@
 package org.ray.housewebscraper.service
 
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.runBlocking
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -10,11 +11,11 @@ class Scheduler(
     private val scraperService: ScraperService,
 ) {
 
-    @Scheduled(fixedDelayString = "PT15M", initialDelayString = "PT5S")
+    @Scheduled(fixedDelayString = "PT1M", initialDelayString = "PT5S")
     fun getLatestHousesByCity() {
         runBlocking {
             scraperService.scrapeHousesForCityInRangeAndSave("Haarlem", 0, 300000, 1)
-//                .onEach {  }
+                .onEach { println("found $it") }
                 .collect()
         }
     }
